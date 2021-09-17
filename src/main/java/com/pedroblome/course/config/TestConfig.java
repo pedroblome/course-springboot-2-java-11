2338345c6d183123f60a8ee22d78402a39418b46
pedroblome <joaopedroblome78@gmail.com>
2021-09-17 00:15:39 -0300

    Product

--- src/main/java/com/pedroblome/course/config/TestConfig.java
+++ src/main/java/com/pedroblome/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.pedroblome.course.entities.Category;
 import com.pedroblome.course.entities.Order;
+import com.pedroblome.course.entities.Product;
 import com.pedroblome.course.entities.User;
 import com.pedroblome.course.entities.enums.OrderStatus;
 import com.pedroblome.course.repositories.CategoryRepository;
 import com.pedroblome.course.repositories.OrderRepository;
+import com.pedroblome.course.repositories.ProductRepository;
 import com.pedroblome.course.repositories.UserRepository;
 
 @Configuration
@@ -25,6 +27,8 @@ public class TestConfig implements CommandLineRunner {
 	private OrderRepository orderRepository;
 	@Autowired
 	private CategoryRepository categoryRespository;
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -44,6 +48,13 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1); 
 				
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		
 		
